a5b58caca2793891794ba16f7c15ab0ea555e49d
Matthew Smith <mattdns100689@gmail.com>
2016-09-15 10:51:41 +0100

    Sorted cropper

--- whaleFolders.py
+++ whaleFolders.py
@@ -8,24 +8,26 @@ allImgs = glob.glob("w_*")
 trCsv = pd.read_csv("../train.csv")
 
 # Make whale directories 
-whales = trCsv.whaleID.unique()
-for whale in whales:
-    if not os.path.exists(whale):
-        os.makedirs(whale)
+def makeDirs():
+    whales = trCsv.whaleID.unique()
+    for whale in whales:
+        if not os.path.exists(whale):
+            os.makedirs(whale)
 
-if not os.path.exists("test"):
-    os.makedirs("test")
-testImages = []
+    if not os.path.exists("test"):
+        os.makedirs("test")
 
-count = 0 
-trImgs = []
-teImgs = []
-for img in allImgs:
-    obs = trCsv[trCsv.Image == img]
-    if len(obs) > 0: # Then its a training item
-        pass
-        #os.rename(obs.Image.values[0], obs.whaleID.values[0] + "/" + obs.Image.values[0])
-    else:
-        os.rename(img,"test/"+img)
+
+def allocateToDirs(): # allocates the whale into its designated whale folder
+    testImages = []
+    trImgs = []
+    teImgs = []
+    for img in allImgs:
+        obs = trCsv[trCsv.Image == img]
+        if len(obs) > 0: # Then its a training item as its not in train csv.
+            os.rename(obs.Image.values[0], obs.whaleID.values[0] + "/" + obs.Image.values[0])
+        else:
+            pass
+            os.rename(img,"test/"+img)
 
 
